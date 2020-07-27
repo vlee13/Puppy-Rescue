1ecdf89e007fe4f46112d560e0c6d278944df2ae
vlee13 <vlee13@gmail.com>
2020-07-27 15:16:31 -0700

    components made

---
+++ frontend/src/components/MeetTeam.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MeetTeam = () => {
+  return (
+    <div>
+      <div>Valerie, Carlos, DP</div>
+      <div>
+        Have links to our LinkedIn and GitHub and how we came together for a
+        great cause
+      </div>
+    </div>
+  );
+};
+
+export default MeetTeam;
