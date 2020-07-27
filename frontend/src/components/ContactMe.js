18642ae195bd54a882f64c5dc9d012d9aa8bdccc
vlee13 <vlee13@gmail.com>
2020-07-27 15:16:31 -0700

    components made

---
+++ frontend/src/components/ContactMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactMe = () => {
+  return <div>Email form</div>;
+};
+
+export default ContactMe;
---
+++ frontend/src/components/TenDogs.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TenDogs extends Component {
+  render() {
+    return <div>List of 10 dogs selected from LA shelter</div>;
+  }
+}
+
+export default TenDogs;
