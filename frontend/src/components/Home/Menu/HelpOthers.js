40f95ecd0cc0a0189188770fa0a45974e41f9ddf
Carlos Cesar Valle <carloscesarvalle@Carloss-iMac.local>
2020-07-30 14:00:28 -0400

    ReactJS Backbones Ready

--- frontend/src/components/Home/Menu/HelpOthers.js
+++ frontend/src/components/Home/Menu/HelpOthers.js
@@ -1,7 +1,250 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const HelpOthers = () => {
-  return <div>Here are other organizations to help</div>;
-};
+class HelpOthers extends Component {
+  state = {
+    showNavbar: true,
+    showPage: true,
+    showLogIn: false,
+    showSignUp: false,
+    showMenu: false,
+    organizations: [],
+  };
+
+  // async componentDidMount() {
+  //   let res = await actions.getOrganizations();
+  //   console.log("hello", res);
+  //   this.setState({
+  //     oraganizations: res.data.oraganizations,
+  //   });
+  // }
+
+  displayNavBar = () => {
+    return (
+      <div>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            this.setState({
+              showLogIn: true,
+              showPage: false,
+              showNavbar: false,
+            });
+          }}
+        >
+          Log in
+        </button>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            this.setState({
+              showMenu: true,
+              showPage: false,
+              showNavbar: false,
+            });
+          }}
+        >
+          Menu
+        </button>
+      </div>
+    );
+  };
+
+  displayMenu = () => {
+    return (
+      <div>
+        <button
+          onClick={() =>
+            this.setState({
+              showMenu: false,
+              showNavbar: true,
+            })
+          }
+        >
+          X
+        </button>
+        <br />
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+        <Link to="/about">
+          <div>About</div>
+        </Link>
+        <Link to="/fosterdog">
+          <div>The dog I'm fostering now</div>
+        </Link>
+        <Link to="/vote">
+          <div>Vote for the next dog I'll foster</div>
+        </Link>
+        <button
+          onClick={() =>
+            this.setState({
+              showMenu: false,
+              showNavbar: true,
+            })
+          }
+        >
+          Help other organizations
+        </button>
+        <Link to="/game">
+          <div>
+            Play our 90's inspired game <i>Dog Zoey in Space</i>
+          </div>
+        </Link>
+        <Link to="/meetteam">
+          <div>Meet the team</div>
+        </Link>
+        <Link to="/contactus">
+          <div>Contact us</div>
+        </Link>
+      </div>
+    );
+  };
+
+  displayLogIn = () => {
+    return (
+      <div>
+        <div ClassName="SixImages">
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              this.setState({
+                showPage: true,
+                showLogIn: false,
+                showNavbar: true,
+              });
+              console.log("Hi");
+            }}
+          >
+            BACK
+          </button>
+          <div>
+            <img src="" alt="" ClassName="logoMark" />
+          </div>
+          <h1>Log in to Valerie's Foster Dog</h1>
+          <h2>Enter details below</h2>
+          <label>Email adress</label>
+          <input type="text" id="fname" name="fname" value="" />
+          <br />
+          <label>Password</label>
+          <Link>Forgot your password?</Link>
+          <input type="password" id="fname" name="fpassword" value="" />
+          <br />
+          <button>LOG IN</button>
+          <button
+            onClick={() => {
+              this.setState({
+                showPage: false,
+                showLogIn: false,
+                showSignUp: true,
+                showNavbar: false,
+              });
+            }}
+          >
+            Don't have an account?
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  displaySignUp = () => {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              showPage: false,
+              showLogIn: true,
+              showSignUp: false,
+              showNavbar: false,
+            });
+            console.log("Hi");
+          }}
+        >
+          BACK
+        </button>
+        <img src="" alt="" ClassName="logoMark" />
+        <h1>Be part of our community</h1>
+        <label>Name</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <label>Email adress</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <label>Create a password</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <button>SIGN UP</button>
+        <button
+          onClick={() => {
+            this.setState({
+              showPage: false,
+              showLogIn: true,
+              showSignUp: false,
+              showNavbar: false,
+            });
+            console.log("Hi");
+          }}
+        >
+          <p>Already have an account?</p>
+        </button>
+      </div>
+    );
+  };
+
+  displayHeader = () => {
+    return (
+      <div>
+        <img src="" alt="logoMark" className="LogoMark" />
+        <span className="header">Help others</span>
+        <hr></hr>
+      </div>
+    );
+  };
+
+  displayOrganizations = () => {
+    return (
+      <div>
+        <p>I'm waiting for Organizations from Database</p>
+      </div>
+    );
+    //   return this.state.organizations.map((eachOrganization) => {
+    //     return (
+    //       <div>
+    //         <img src={eachOrganization.image} alt="organizationPic" className="organizationPic" />
+    //         <br />
+    //         <h1>{eachOrganization.name}</h1>
+    //         <ul>{`Website: ${eachOrganization.website}`}</ul>
+    //         <p>{eachOrganization.description}</p>
+    //       </div>
+    //     );
+    //   });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.showNavbar ? this.displayNavBar() : ""}
+        {this.state.showLogIn & !this.state.showMenu ? this.displayLogIn() : ""}
+        {this.state.showSignUp ? this.displaySignUp() : ""}
+        {this.state.showMenu & !this.state.showLogIn ? this.displayMenu() : ""}
+        {!this.state.showLogIn & !this.state.showMenu & !this.state.showSignUp
+          ? this.displayHeader()
+          : ""}
+        {!this.state.showLogIn & !this.state.showMenu & !this.state.showSignUp
+          ? this.displayOrganizations()
+          : ""}
+      </div>
+    );
+  }
+}
 
 export default HelpOthers;
--- frontend/src/components/Home/Menu/Vote.js
+++ frontend/src/components/Home/Menu/Vote.js
@@ -1,42 +1,250 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import actions from "../../../services/index.js";
 
 class Vote extends Component {
   state = {
+    showNavbar: true,
+    showPage: true,
+    showLogIn: false,
+    showSignUp: false,
+    showMenu: false,
     dogs: [],
   };
 
-  async componentDidMount() {
-    let res = await actions.getDogs();
-    console.log("hello", res);
-    this.setState({
-      dogs: res.data.dogs,
-    });
-  }
+  // async componentDidMount() {
+  //   let res = await actions.getDogs();
+  //   console.log("hello", res);
+  //   this.setState({
+  //     dogs: res.data.dogs,
+  //   });
+  // }
 
+  displayNavBar = () => {
+    return (
+      <div>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            this.setState({
+              showLogIn: true,
+              showPage: false,
+              showNavbar: false,
+            });
+          }}
+        >
+          Log in
+        </button>
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            this.setState({
+              showMenu: true,
+              showPage: false,
+              showNavbar: false,
+            });
+          }}
+        >
+          Menu
+        </button>
+      </div>
+    );
+  };
+
+  displayMenu = () => {
+    return (
+      <div>
+        <button
+          onClick={() =>
+            this.setState({
+              showMenu: false,
+              showNavbar: true,
+            })
+          }
+        >
+          X
+        </button>
+        <br />
+        <Link to="/">
+          <div>Home</div>
+        </Link>
+        <Link to="/about">
+          <div>About</div>
+        </Link>
+        <Link to="/fosterdog">
+          <div>The dog I'm fostering now</div>
+        </Link>
+        <button
+          onClick={() =>
+            this.setState({
+              showMenu: false,
+              showNavbar: true,
+            })
+          }
+        >
+          Vote for the next dog I'll foster
+        </button>
+        <Link to="/helpothers">
+          <div>Help other organizations</div>
+        </Link>
+        <Link to="/game">
+          <div>
+            Play our 90's inspired game <i>Dog Zoey in Space</i>
+          </div>
+        </Link>
+        <Link to="/meetteam">
+          <div>Meet the team</div>
+        </Link>
+        <Link to="/contactus">
+          <div>Contact us</div>
+        </Link>
+      </div>
+    );
+  };
+
+  displayLogIn = () => {
+    return (
+      <div>
+        <div ClassName="SixImages">
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+          <img src="" alt="" ClassName="Image" />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              this.setState({
+                showPage: true,
+                showLogIn: false,
+                showNavbar: true,
+              });
+              console.log("Hi");
+            }}
+          >
+            BACK
+          </button>
+          <div>
+            <img src="" alt="" ClassName="logoMark" />
+          </div>
+          <h1>Log in to Valerie's Foster Dog</h1>
+          <h2>Enter details below</h2>
+          <label>Email adress</label>
+          <input type="text" id="fname" name="fname" value="" />
+          <br />
+          <label>Password</label>
+          <Link>Forgot your password?</Link>
+          <input type="password" id="fname" name="fpassword" value="" />
+          <br />
+          <button>LOG IN</button>
+          <button
+            onClick={() => {
+              this.setState({
+                showPage: false,
+                showLogIn: false,
+                showSignUp: true,
+                showNavbar: false,
+              });
+            }}
+          >
+            Don't have an account?
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  displaySignUp = () => {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              showPage: false,
+              showLogIn: true,
+              showSignUp: false,
+              showNavbar: false,
+            });
+            console.log("Hi");
+          }}
+        >
+          BACK
+        </button>
+        <img src="" alt="" ClassName="logoMark" />
+        <h1>Be part of our community</h1>
+        <label>Name</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <label>Email adress</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <label>Create a password</label>
+        <input type="text" id="fname" name="fname" value="" />
+        <br />
+        <button>SIGN UP</button>
+        <button
+          onClick={() => {
+            this.setState({
+              showPage: false,
+              showLogIn: true,
+              showSignUp: false,
+              showNavbar: false,
+            });
+            console.log("Hi");
+          }}
+        >
+          <p>Already have an account?</p>
+        </button>
+      </div>
+    );
+  };
+
+  displayHeader = () => {
+    return (
+      <div>
+        <img src="" alt="logoMark" className="LogoMark" />
+        <span className="header">Vote for the next dog I'll foster</span>
+        <hr></hr>
+      </div>
+    );
+  };
   displayDogs = () => {
-    return this.state.dogs.map((eachDog) => {
-      return (
-        <li>
-          {/* <img src={eachDog.image} alt="dogpic" /> */}
-          {eachDog.name}
-          {`#${eachDog.shelterID}`}
-          <br />
-          {eachDog.description}
-          <br />
-          {`Age: ${eachDog.age}`}
-          <br />
-          {`Weight: ${eachDog.weight}`}
-        </li>
-      );
-    });
+    return (
+      <div>
+        <p>I'm waiting for the 10 dogs from Database</p>
+      </div>
+    );
+    //   return this.state.dogs.map((eachDog) => {
+    //     return (
+    //       <div>
+    //         <img src={eachDog.image} alt="dogpic" className="dogToFoster" />
+    //         <br />
+    //         <h1>{eachDog.name}</h1>
+    //         <div>votes</div>
+    //         <ul>{`Shelter Id: ${eachDog.shelterID}`}</ul>
+    //         <ul>{`Age: ${eachDog.age}`}</ul>
+    //         <ul>{`Weight: ${eachDog.weight}`}</ul>
+    //         <p>{eachDog.description}</p>
+    //       </div>
+    //     );
+    //   });
   };
 
   render() {
     return (
       <div>
-        {/* {this.displayDogs()} */}
-        Here are 10 dogs to vote for
+        {this.state.showNavbar ? this.displayNavBar() : ""}
+        {this.state.showLogIn & !this.state.showMenu ? this.displayLogIn() : ""}
+        {this.state.showSignUp ? this.displaySignUp() : ""}
+        {this.state.showMenu & !this.state.showLogIn ? this.displayMenu() : ""}
+        {!this.state.showLogIn & !this.state.showMenu & !this.state.showSignUp
+          ? this.displayHeader()
+          : ""}
+        {!this.state.showLogIn & !this.state.showMenu & !this.state.showSignUp
+          ? this.displayDogs()
+          : ""}
       </div>
     );
   }
