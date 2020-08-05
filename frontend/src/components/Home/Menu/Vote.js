4032cef65482bfecf07bf14f28dc91a995612125
Carlos Cesar Valle <carloscesarvalle@Carloss-iMac.local>
2020-08-05 13:05:05 -0400

    Pages with 75% CSS

--- frontend/src/components/Home/Menu/Vote.js
+++ frontend/src/components/Home/Menu/Vote.js
@@ -20,7 +20,258 @@ class Vote extends Component {
     });
   }
 
-  displayDogs = () => {
+  //This is the old NavBar for the burger. I'm commenting out this just in case backend needs it
+
+  // displayNavBar = () => {
+  //   return (
+  //     <div>
+  //       <button
+  //         onClick={(event) => {
+  //           event.preventDefault();
+  //           this.setState({
+  //             showLogIn: true,
+  //             showPage: false,
+  //             showNavbar: false,
+  //           });
+  //         }}
+  //       >
+  //         Log in
+  //       </button>
+  //       <button
+  //         onClick={(event) => {
+  //           event.preventDefault();
+  //           this.setState({
+  //             showMenu: true,
+  //             showPage: false,
+  //             showNavbar: false,
+  //           });
+  //         }}
+  //       >
+  //         Menu
+  //       </button>
+  //     </div>
+  //   );
+  // };
+
+  // displayMenu = () => {
+  //   return (
+  //     <div>
+  //       <Link
+  //         onClick={() =>
+  //           this.setState({
+  //             showMenu: false,
+  //             showNavbar: true,
+  //           })
+  //         }
+  //       >
+  //         X
+  //       </Link>
+  //       <br />
+  //       <Link to="/">
+  //         <div>Home</div>
+  //       </Link>
+  //       <Link to="/about">
+  //         <div>About</div>
+  //       </Link>
+  //       <Link to="/fosterdog">
+  //         <div>The dog I'm fostering now</div>
+  //       </Link>
+  //       <Link
+  //         onClick={() =>
+  //           this.setState({
+  //             showMenu: false,
+  //             showNavbar: true,
+  //           })
+  //         }
+  //       >
+  //         Vote for the next dog I'll foster
+  //       </Link>
+  //       <Link to="/helpothers">
+  //         <div>Help other organizations</div>
+  //       </Link>
+  //       <Link to="/game">
+  //         <div>
+  //           Play our 90's inspired game <i>Dog Zoey in Space</i>
+  //         </div>
+  //       </Link>
+  //       <Link to="/meetteam">
+  //         <div>Meet the team</div>
+  //       </Link>
+  //       <Link to="/contactus">
+  //         <div>Contact us</div>
+  //       </Link>
+  //     </div>
+  //   );
+  // };
+
+  // displayLogIn = () => {
+  //   return (
+  //     <div>
+  //       <div ClassName="SixImages">
+  //         <img src="" alt="" ClassName="Image" />
+  //         <img src="" alt="" ClassName="Image" />
+  //         <img src="" alt="" ClassName="Image" />
+  //         <img src="" alt="" ClassName="Image" />
+  //         <img src="" alt="" ClassName="Image" />
+  //         <img src="" alt="" ClassName="Image" />
+  //       </div>
+  //       <div>
+  //         <button
+  //           onClick={() => {
+  //             this.setState({
+  //               showPage: true,
+  //               showLogIn: false,
+  //               showNavbar: true,
+  //             });
+  //             console.log("Hi");
+  //           }}
+  //         >
+  //           BACK
+  //         </button>
+  //         <div>
+  //           <img src="" alt="" ClassName="logoMark" />
+  //         </div>
+  //         <h1>Log in to Valerie's Foster Dog</h1>
+  //         <h2>Enter details below</h2>
+  //         <label>Email adress</label>
+  //         <input type="text" id="fname" name="fname" value="" />
+  //         <br />
+  //         <label>Password</label>
+  //         <Link>Forgot your password?</Link>
+  //         <input type="password" id="fname" name="fpassword" value="" />
+  //         <br />
+  //         <button>LOG IN</button>
+  //         <button
+  //           onClick={() => {
+  //             this.setState({
+  //               showPage: false,
+  //               showLogIn: false,
+  //               showSignUp: true,
+  //               showNavbar: false,
+  //             });
+  //           }}
+  //         >
+  //           Don't have an account?
+  //         </button>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
+  // displaySignUp = () => {
+  //   return (
+  //     <div>
+  //       <button
+  //         onClick={() => {
+  //           this.setState({
+  //             showPage: false,
+  //             showLogIn: true,
+  //             showSignUp: false,
+  //             showNavbar: false,
+  //           });
+  //           console.log("Hi");
+  //         }}
+  //       >
+  //         BACK
+  //       </button>
+  //       <img src="" alt="" ClassName="logoMark" />
+  //       <h1>Be part of our community</h1>
+  //       <label>Name</label>
+  //       <input type="text" id="fname" name="fname" value="" />
+  //       <br />
+  //       <label>Email adress</label>
+  //       <input type="text" id="fname" name="fname" value="" />
+  //       <br />
+  //       <label>Create a password</label>
+  //       <input type="text" id="fname" name="fname" value="" />
+  //       <br />
+  //       <button>SIGN UP</button>
+  //       <button
+  //         onClick={() => {
+  //           this.setState({
+  //             showPage: false,
+  //             showLogIn: true,
+  //             showSignUp: false,
+  //             showNavbar: false,
+  //           });
+  //           console.log("Hi");
+  //         }}
+  //       >
+  //         <p>Already have an account?</p>
+  //       </button>
+  //     </div>
+  //   );
+  // };
+
+  displayNavBar = () => {
+    return (
+      <div className="Navbar">
+        <div className="NavbarContainer">
+          <Link to="/" className="link">
+            <div className="NavBarElement">
+              <img id="LogoMark" />
+              {/* This span is just a placeholder. It would be remove after finishing horizontal logo */}
+              <span className="NavBarElementText">House of Paws</span>
+              {/* Remove till here */}
+            </div>
+          </Link>
+          <div className="NavBarElement">
+            <Link to="/about" className="link">
+              <div className="NavBarElementText">About us</div>
+            </Link>
+            <Link to="/fosterdog" className="link">
+              <div className="NavBarElementText">Foster dogs</div>
+            </Link>
+
+            <Link to="/vote" className="link">
+              <div className="NavBarElementText">Vote!</div>
+            </Link>
+
+            <Link to="/adopted" className="link">
+              <div className="NavBarElementText">Adopted</div>
+            </Link>
+
+            <Link to="/contactus" className="link">
+              <div className="NavBarElementText">Contact us</div>
+            </Link>
+            <Link to="/contactus" className="link">
+              <div className="NavBarElementText">|</div>
+            </Link>
+            <button
+              className="navBarButton"
+              onClick={(event) => {
+                event.preventDefault();
+                this.setState({
+                  showLogIn: true,
+                  showPage: false,
+                  showNavbar: false,
+                });
+              }}
+            >
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  displayHeader = () => {
+    return (
+      <div className="container">
+        <img className="imageForVote" />
+        <div className="textForHeroImage">
+          <h1>Vote.</h1>
+          <p>
+            We can only take one dog to be fostered in this house. Help us
+            choose! You have three votes.
+          </p>
+        </div>
+      </div>
+    );
+  };
+
+  displayDogsToVote = () => {
     return this.state.dogs
       .filter((eachDog) => eachDog.status === "available")
       .map((eachDog, i) => {
@@ -54,203 +305,6 @@ class Vote extends Component {
       });
   };
 
-  displayNavBar = () => {
-    return (
-      <div>
-        <button
-          onClick={(event) => {
-            event.preventDefault();
-            this.setState({
-              showLogIn: true,
-              showPage: false,
-              showNavbar: false,
-            });
-          }}
-        >
-          Log in
-        </button>
-        <button
-          onClick={(event) => {
-            event.preventDefault();
-            this.setState({
-              showMenu: true,
-              showPage: false,
-              showNavbar: false,
-            });
-          }}
-        >
-          Menu
-        </button>
-      </div>
-    );
-  };
-
-  displayMenu = () => {
-    return (
-      <div>
-        <Link
-          onClick={() =>
-            this.setState({
-              showMenu: false,
-              showNavbar: true,
-            })
-          }
-        >
-          X
-        </Link>
-        <br />
-        <Link to="/">
-          <div>Home</div>
-        </Link>
-        <Link to="/about">
-          <div>About</div>
-        </Link>
-        <Link to="/fosterdog">
-          <div>The dog I'm fostering now</div>
-        </Link>
-        <Link
-          onClick={() =>
-            this.setState({
-              showMenu: false,
-              showNavbar: true,
-            })
-          }
-        >
-          Vote for the next dog I'll foster
-        </Link>
-        <Link to="/helpothers">
-          <div>Help other organizations</div>
-        </Link>
-        <Link to="/game">
-          <div>
-            Play our 90's inspired game <i>Dog Zoey in Space</i>
-          </div>
-        </Link>
-        <Link to="/meetteam">
-          <div>Meet the team</div>
-        </Link>
-        <Link to="/contactus">
-          <div>Contact us</div>
-        </Link>
-      </div>
-    );
-  };
-
-  displayLogIn = () => {
-    return (
-      <div>
-        <div ClassName="SixImages">
-          <img src="" alt="" ClassName="Image" />
-          <img src="" alt="" ClassName="Image" />
-          <img src="" alt="" ClassName="Image" />
-          <img src="" alt="" ClassName="Image" />
-          <img src="" alt="" ClassName="Image" />
-          <img src="" alt="" ClassName="Image" />
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              this.setState({
-                showPage: true,
-                showLogIn: false,
-                showNavbar: true,
-              });
-              console.log("Hi");
-            }}
-          >
-            BACK
-          </button>
-          <div>
-            <img src="" alt="" ClassName="logoMark" />
-          </div>
-          <h1>Log in to Valerie's Foster Dog</h1>
-          <h2>Enter details below</h2>
-          <label>Email adress</label>
-          <input type="text" id="fname" name="fname" value="" />
-          <br />
-          <label>Password</label>
-          <Link>Forgot your password?</Link>
-          <input type="password" id="fname" name="fpassword" value="" />
-          <br />
-          <button>LOG IN</button>
-          <button
-            onClick={() => {
-              this.setState({
-                showPage: false,
-                showLogIn: false,
-                showSignUp: true,
-                showNavbar: false,
-              });
-            }}
-          >
-            Don't have an account?
-          </button>
-        </div>
-      </div>
-    );
-  };
-
-  displaySignUp = () => {
-    return (
-      <div>
-        <button
-          onClick={() => {
-            this.setState({
-              showPage: false,
-              showLogIn: true,
-              showSignUp: false,
-              showNavbar: false,
-            });
-            console.log("Hi");
-          }}
-        >
-          BACK
-        </button>
-        <img src="" alt="" ClassName="logoMark" />
-        <h1>Be part of our community</h1>
-        <label>Name</label>
-        <input type="text" id="fname" name="fname" value="" />
-        <br />
-        <label>Email adress</label>
-        <input type="text" id="fname" name="fname" value="" />
-        <br />
-        <label>Create a password</label>
-        <input type="text" id="fname" name="fname" value="" />
-        <br />
-        <button>SIGN UP</button>
-        <button
-          onClick={() => {
-            this.setState({
-              showPage: false,
-              showLogIn: true,
-              showSignUp: false,
-              showNavbar: false,
-            });
-            console.log("Hi");
-          }}
-        >
-          <p>Already have an account?</p>
-        </button>
-      </div>
-    );
-  };
-
-  displayHeader = () => {
-    return (
-      <div>
-        <img src="" alt="logoMark" className="LogoMark" />
-        <div>
-          <h2>
-            Welcome to the voting stage! You have up to three votes to choose
-            three different puppies. The one with the most votes gets to go home
-            with us! Thank you for participating and making a difference.
-          </h2>
-        </div>
-        <hr></hr>
-      </div>
-    );
-  };
-
   vote = (eachDog, i) => {
     actions
       .vote(eachDog)
@@ -269,10 +323,153 @@ class Vote extends Component {
       .catch((err) => console.error(err));
     console.log(eachDog);
   };
+
+  displayFooter = () => {
+    return (
+      <div className="footer">
+        {/* Main Container for 3 floats: Dev Team, Navigation and Other links */}
+
+        <div className="footerContainer">
+          <div className="floatElement">
+            <img class="footerLogoMark" />
+            <div className="address">
+              House of Valerie Apt. 911, <br />
+              Kimchi 00911 USA
+            </div>
+            <div classNAme="phoneNumber"> 0-800-WeLoveDogs</div>
+
+            <span>
+              <img id="socialMediaIcon" />
+            </span>
+            <span>
+              <img id="socialMediaIcon" />
+            </span>
+            <span>
+              <img id="socialMediaIcon" />
+            </span>
+            <span>
+              <img id="socialMediaIcon" />
+            </span>
+          </div>
+
+          <div className="floatElement">
+            <h4 className="title">Development Team</h4>
+            {/* Carlos */}
+            <div>Carlos Cesar Valle</div>
+            <div>
+              Software Developer <br /> UX & Branding Designer
+            </div>
+            {/* <p className="meetTeamText">From: Miami, FL</p> */}
+            <span>
+              <a href="https://github.com/carloscesarvalle" target="_blank">
+                GitHub
+              </a>
+            </span>
+            <span>|</span>
+            <span>
+              <a
+                href="https://www.linkedin.com/in/carloscesarvalle/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </span>
+            <span>|</span>
+            <span>
+              <a
+                href="https://www.behance.net/carloscesarvalle"
+                target="_blank"
+              >
+                Behance
+              </a>
+            </span>
+            {/* Valerie */}
+            <div>Valerie Lee Kang</div>
+            <div>
+              Software Developer <br /> Canvas Developer
+            </div>
+            <span>
+              <a href="https://github.com/vlee13" target="_blank">
+                GitHub
+              </a>
+            </span>
+            <span>|</span>
+            <span>
+              <a href="https://www.linkedin.com/in/vlee13/" target="_blank">
+                LinkedIn
+              </a>
+            </span>
+            {/* DP */}
+            <div>Dionel Panuncia</div>
+            <div>Software Developer</div>
+            <span>
+              <a href="https://github.com/dpanuncia/" target="_blank">
+                GitHub
+              </a>
+            </span>
+            <span>|</span>
+            <span>
+              <a href="https://www.linkedin.com/in/dpanuncia/" target="_blank">
+                LinkedIn
+              </a>
+            </span>
+          </div>
+
+          {/* Navigation Bar */}
+
+          <div className="floatElement">
+            <h4 className="title">Navigation</h4>
+
+            <Link to="/fosterdog" className="footerLink">
+              Foster dogs
+            </Link>
+
+            <Link to="/vote" className="footerLink">
+              Vote!
+            </Link>
+
+            <Link to="/helpothers" className="footerLink">
+              Help others
+            </Link>
+
+            <Link to="/contactus" className="footerLink">
+              Contact us
+            </Link>
+
+            <h4 className="title">Other links</h4>
+            <Link to="/game" className="footerLink">
+              Zoey 90's Game
+            </Link>
+          </div>
+
+          {/* Newsletter and Copyright */}
+          <div className="floatElement">
+            <div>Do you want to receive our monthly newsletter?</div>
+            <button
+              className="footerButton"
+              onClick={(event) => {
+                event.preventDefault();
+                console.log("clicking");
+              }}
+            >
+              Sign Up
+            </button>
+            <div className="footerText">
+              House of Paws was created with a lot of love during Ironhack
+              fulltime web development bootcamp. <br /> All rights reserved.{" "}
+              <br /> We also want to show our appreciation to the photograpers.
+              Most of their work can be found on pexels.com and unsplash.com.
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
-        {this.state.showNavbar ? this.displayNavBar() : ""}
+        {/* {this.state.showNavbar ? this.displayNavBar() : ""}
         {this.state.showLogIn & !this.state.showMenu ? this.displayLogIn() : ""}
         {this.state.showSignUp ? this.displaySignUp() : ""}
         {this.state.showMenu & !this.state.showLogIn ? this.displayMenu() : ""}
@@ -281,7 +478,11 @@ class Vote extends Component {
           : ""}
         {!this.state.showLogIn & !this.state.showMenu & !this.state.showSignUp
           ? this.displayDogs()
-          : ""}
+          : ""} */}
+        {this.displayNavBar()}
+        {this.displayHeader()}
+        {this.displayDogsToVote()}
+        {this.displayFooter()}
       </div>
     );
   }
